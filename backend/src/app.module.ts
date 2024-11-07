281b81a6b1baad18a7bef7103b99a6f5f6f30c42
eldadstar <eldadfikre456@gmail.com>
2024-11-07 12:26:04 +0300

    updated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,30 +1,41 @@
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev": ".env.test" 
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev": ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod" ,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+        .valid('dev','prod')
+        .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required()
+      })
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
       introspection: true,
-      playground: true
+      playground: true,
     }),          
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username:'postgres',
-      password:'123456',
-      database:"uber-eats",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: "postgres",
+      password: "123456",
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true 
     }),
